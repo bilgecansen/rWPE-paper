6cf183a8b06a205a537207dc40fb7f645d255821
bilgecan <bilgecan.sen@gmail.com>
2023-06-28 14:08:41 -0400

    update simulation code

--- simulations.R
+++ simulations.R
@@ -8,60 +8,42 @@ source("functions_pop.R")
 
 # Permutations with white noise -------------------------------------------
 
-ss <- c(10,20,30,40)
+ss <- c(10, 20, 30, 40)
 
-# Chaos
-r_ch <- c(2.8, 2.9, 3.0, 3.1, 3.3, 3.4) 
+# Chaos and point cycles
+r_ch <- c(2.4, 2.6, 2.85, 3.0, 3.14, 3.3) 
 
 wpe_ch <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_ch, perm = "white")
-
-pe_ch <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_ch, perm = "white", type = "PE")
-
-# Point cycles
-r_pc <- c(2.4, 2.5, 2.6) 
-
-wpe_pc <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_pc, perm = "white")
-
-pe_pc <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_pc, perm = "white", type = "PE")
+  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), r = r_ch, perm = "white")
 
 # Equilibrium dynamics
 r_eq <- seq(0.1, 1.9, 0.3)
 
 wpe_eq <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_eq, perm = "white")
+  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), r = r_eq, perm = "white")
 
-pe_eq <- foreach(k = 1:length(ss)) %do% 
-  get_pe_rick(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_eq, perm = "white", type = "PE")
+# Equilibrium dynamics with trend
+wpe_eq_cc <- foreach(k = 1:length(ss)) %do% 
+  get_pe_rick_cc(ss[k], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), r = r_eq, perm = "white")
 
 # Trends
-r_tr <- seq(0, 1, 0.2) 
+r_tr <- c(0, 0.01, 0.1, 0.2, 0.4) 
 
 wpe_tr <- foreach(k = 1:length(ss)) %do% 
-  get_pe_exp(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_tr, perm = "white")
-
-pe_tr <- foreach(k = 1:length(ss)) %do% 
-  get_pe_exp(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), r = r_tr, perm = "white", type = "PE")
+  get_pe_exp(ss[k], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), r = r_tr, perm = "white")
 
 # White noise
 wpe_wh <- foreach(k = 1:length(ss)) %do%  
-  get_pe_white(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), perm = "white")
-
-pe_wh <- foreach(k = 1:length(ss)) %do%  
-  get_pe_white(ss[k], sd = c(0.001, 0.01, 0.1, 0.5), perm = "white", type = "PE")
+  get_pe_white(ss[k], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), perm = "white")
 
 res <- list(wpe_ch = wpe_ch,
-            pe_ch = pe_ch,
-            wpe_pc = wpe_pc,
-            pe_pc = pe_pc,
             wpe_eq = wpe_eq,
-            pe_eq = pe_eq,
+            wpe_eq_cc = wpe_eq_cc,
             wpe_tr = wpe_tr,
-            pe_tr = pe_tr,
-            wpe_wh = wpe_wh,
-            pe_wh = pe_wh)
+            wpe_wh = wpe_wh)
 
 saveRDS(res, "results_sim.rds")
+
+
+acf_tr <- foreach(k = 1:length(ss)) %do% 
+  get_acf_exp(ss[1], sd = c(0.001, 0.01, 0.1, 0.2, 0.4), r = r_tr[1])
